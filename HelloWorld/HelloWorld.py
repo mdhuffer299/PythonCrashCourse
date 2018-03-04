32bd5daad30af5a438ece72cfa64800bad38bc84
mdhuffer299 <33228313+mdhuffer299@users.noreply.github.com>
2018-03-04 15:26:34 -0500

    FirstCommit
    Hello World Scripts!

---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  4 15:17:28 2018
+
+@author: mhuffer
+"""
+
+print("Hello Python World!")
---
+++ HelloWorld/HelloWorldVariables.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  4 15:20:08 2018
+
+@author: mhuffer
+"""
+
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
